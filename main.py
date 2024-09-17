bf3068ad626bace117dda2835cfffb10ce494a4a
David Ogunmola <127635293+kdavid001@users.noreply.github.com>
2024-09-17 21:42:56 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,15 @@
+import requests
+from bs4 import BeautifulSoup
+
+URL = "https://web.archive.org/web/20200518073855/https://www.empireonline.com/movies/features/best-movies-2/"
+
+# Write your code below this line 👇
+soup = BeautifulSoup(requests.get(URL).text, "html.parser")
+article = [_.getText() for _ in soup.find_all(name="h3", class_="title")]
+
+print(f"The top 100 movies to watch from the best to the list:")
+with open("movies to watch.txt", "w") as file:
+    for n in range(0, len(article)):
+        movies_watch = (article[-n - 1])
+        print(movies_watch)
+        file.write(movies_watch + "\n")
